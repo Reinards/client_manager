ed9f2c06ec53e14ad620046aab761537bf8d1fd0
Reinards <reinardssaulitis@gmail.com>
2019-05-06 15:32:54 +0300

    Add gh-pages branch

---
+++ precache-manifest.9170c020101c4205cf7e38564e88d17a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a798cdadc7cd321e3f72425b70bface",
+    "url": "./static/media/OpenSans-Regular.5a798cda.ttf"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "cc56a3fde719f817f0e7",
+    "url": "./static/js/main.c388b656.chunk.js"
+  },
+  {
+    "revision": "68e6f2cc2ca15d12faf3",
+    "url": "./static/js/2.b930a592.chunk.js"
+  },
+  {
+    "revision": "cc56a3fde719f817f0e7",
+    "url": "./static/css/main.63f686a6.chunk.css"
+  },
+  {
+    "revision": "da399dbb5013c85207bffda0b36a5eba",
+    "url": "./index.html"
+  }
+];
